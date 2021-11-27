d488b503bf3089a9e9ac49b2a2d629db6f2e3e1c
kalimi03 <kalimi03@gmail.com>
2021-11-27 08:25:11 +0000

    update model name

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -5,11 +5,10 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "best-modl"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
 logs = service.get_logs()
-
 for line in logs.split('\n'):
     print(line)
